f22fb2e70979e3f145d8696c108746111fe28bb7
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-11-09 14:56:12 -0800

    added page content

---
+++ src/assets/app/layout.jsx
@@ -0,0 +1,7 @@
+export default function FooBarBaz({children}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/assets/app/page.jsx
@@ -0,0 +1,3 @@
+export default function Whatever() {
+  return <h1>Hi there, everything's working great!</h1>
+}
